df6c92ee19148425fae8b602d73174e18d5a589e
Menno <mennovdeerden@gmail.com>
2022-06-28 02:23:07 +0200

    Finished prototype

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,16 @@
 &-
 */
 
-use rand::Rng;
+use rand::seq::SliceRandom;
 use std::{error::Error, fs, io};
 
 fn main() {
-    // println!(
-    //     "Let's play hangman!\nStart by selecting a difficulty:\n1) easy\n2) medium\n3) hard\n4) EXTREME"
-    // );
+    println!("Welcome to Hangman!\nBegin by entering a character:");
 
-    // // Get difficulty
+    // start live with 6 lives
+    let mut lives: i8 = 6;
+
+    // // get difficulty
     // let mut input = String::new();
     // io::stdin()
     //     .read_line(&mut input)
@@ -23,21 +24,22 @@ fn main() {
     //     Err(err) => panic!("Error ({err}). Failed to parse input ({input}). Try again"),
     // };
 
-    // Get word
+    // get word
     let word = match pick_word() {
         Ok(w) => w,
         Err(e) => panic!("Found error {e:?}"),
     };
 
-    // Create container for the individual letters
+    // create container for the individual letters and a container for the guesses
     let mut container: Vec<char> = vec!['.'; word.len() - 1];
+    let mut guesses: Vec<char> = vec![];
 
-    // Debug
+    // debug
     println!("The word is {word}");
 
     // loop until `death` or win
     loop {
-        // Get input string from user
+        // get input string from user
         let mut guess = String::new();
         match io::stdin().read_line(&mut guess) {
             Ok(n) => {
@@ -48,7 +50,7 @@ fn main() {
             Err(e) => println!("Got error {e} while reading the line."),
         }
 
-        // Convert string input to char
+        // convert string input to char
         let input = match guess.trim().parse::<char>() {
             Ok(input) => {
                 if input.is_alphabetic() {
@@ -61,14 +63,21 @@ fn main() {
             Err(_) => continue,
         };
 
-        // Check if input is in the word
-        if !word.contains(input) {
-            println!("This character is not in the word. Guess again!");
-            // print current word and guesses
-            show_state(&mut container);
+        // update guesses
+        if guesses.contains(&input) {
+            println!("You have already tried this character.");
             continue;
         } else {
-            println!("This character is in the word!");
+            guesses.push(input);
+        }
+        // check if input is in the word
+        if !word.contains(input) {
+            println!("This character is not in the word. Guess again!");
+            lives -= 1;
+            // print current word and guesses
+            show_state(&mut container, &mut guesses, &lives);
+            continue;
+        } else {
             // update container
             for (idx, val) in word.chars().enumerate() {
                 if input == val {
@@ -76,12 +85,52 @@ fn main() {
                     container.remove(idx + 1);
                 }
             }
+
             // print updated word
-            show_state(&mut container);
+            show_state(&mut container, &mut guesses, &lives);
         }
-        if !container.contains(&'.') {
+
+        // check if player should be dead
+        if lives < 0 {
+            println!("You have lost the game lmao.");
             break;
         }
+
+        if !container.contains(&'.') {
+            println!("You guessed the word, you absolute beast!");
+            break;
+        }
+    }
+}
+
+fn _update_guesses(guesses: &mut Vec<char>, input: char, lives: &mut i8) {
+    // update guesses
+    if guesses.contains(&input) {
+        loop {
+            println!("Are you absolutely sure that you want to waste a part of you on a character that you have already tried? (Y/N)");
+            let mut guess = String::new();
+            match io::stdin().read_line(&mut guess) {
+                Ok(n) => {
+                    if n != 3 {
+                        println!("Please enter a single char!")
+                    }
+                }
+                Err(e) => println!("Got error {e} while reading the line."),
+            }
+            if guess.to_lowercase().contains('y') {
+                println!("So be it, you shall lose a limb.");
+                *lives -= 1;
+                break;
+            } else if guess.to_lowercase().contains('n') {
+                println!("You shall be rewarded with mercy.");
+                break;
+            } else {
+                println!("You what?");
+                continue;
+            }
+        }
+    } else {
+        guesses.push(input);
     }
 }
 
@@ -91,28 +140,33 @@ fn main() {
 ///
 /// ~ Use difficulty measure to provide different types of words (length, number of different characters).
 ///
-/// ~ Use Vec.choose instead of get.
-///
-/// ~ Clean up this mess.
 fn pick_word() -> Result<String, Box<dyn Error>> {
-    let whole_list = fs::read_to_string("data/words.txt")?;
-    let word_vec = whole_list.split('\n').collect::<Vec<&str>>();
-    let size = word_vec.len();
-    let idx = rand::thread_rng().gen_range(0..=size);
-    let word = match word_vec.get(idx) {
-        Some(&word) => word,
-        None => panic!(),
+    // import word list
+    let word_list = fs::read_to_string(r"data\words.txt")?;
+    let words = word_list.split('\n').collect::<Vec<&str>>();
+
+    // choose word from list
+    let mut rng = rand::thread_rng();
+    let word = match words.choose(&mut rng) {
+        Some(&chosen_word) => chosen_word,
+        _ => panic!("Something went wrong!"),
     };
     Ok(word.to_string())
 }
 
 /// Given container of current state of the game print the word and guesses
 ///
-fn show_state(container: &mut Vec<char>) {
-    //}, guesses: &Vec<char>) {
+fn show_state(container: &mut Vec<char>, guesses: &mut Vec<char>, lives: &i8) {
     let mut result = String::new();
     for &el in container.clone().iter() {
         result.push(el)
     }
-    println!("{}", result)
+    let mut letters = String::new();
+    for &el in guesses.clone().iter() {
+        letters.push(el)
+    }
+    print!("{}", 27 as char);
+    println!("You have {lives} lives!");
+    println!("The word is: {}", result);
+    println!("Used letters: {}", letters);
 }
